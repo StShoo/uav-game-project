0f8c1f6ca9d53daa7ad9fc0ba54123d6a088f8b3
StShoo <lumen.gaming777@gmail.com>
2022-05-25 22:40:16 +0300

    Added propeller and explosions sounds

--- UAV Game Project/Assets/Scripts/Shooting Scripts/Shoot.cs
+++ UAV Game Project/Assets/Scripts/Shooting Scripts/Shoot.cs
@@ -11,6 +11,8 @@ public class Shoot : MonoBehaviour
 
     public Camera firstPersonCam;
     private GameObject target;
+    public AudioSource rocketShootSound;
+    public AudioSource explosionSound;
 
     // Update is called once per frame
     void Update()
@@ -26,16 +28,23 @@ public class Shoot : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(firstPersonCam.transform.position, firstPersonCam.transform.forward, out hit))
         {
-            if (hit.transform.CompareTag("Enemy"))
-            {
-                Debug.Log(hit.transform.tag + "!!!!");
-            }
-            Debug.Log(hit.transform.tag);
+            StartCoroutine(WaitBeforeExplode(hit));
         }
     }
 
     private void Kill(GameObject target)
     {
         Destroy(target);
+    }
+
+    private IEnumerator WaitBeforeExplode(RaycastHit hit)
+    {
+        rocketShootSound.Play();
+        yield return new WaitForSeconds(1);
+        if (hit.transform.CompareTag("Enemy"))
+        {
+            Kill(hit.transform.gameObject);
+        }
+        explosionSound.Play();
     }
 }
